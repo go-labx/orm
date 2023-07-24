b212971ecba28d7a1df3eefcc5b1f16e19a8041e
kk0829 <244098979@qq.com>
2023-07-24 08:03:13 +0800

    feat: add ToSnakeCase

--- util.go
+++ util.go
@@ -1,11 +1,34 @@
 package orm
 
-import "strings"
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
 
-func mapToString(m map[string]string) string {
+// MapToString is a utility function that converts a map into a string.
+// Each key-value pair in the map is converted into a "key=value" string, and these strings are joined with "&" as the separator.
+// The resulting string does not end with "&".
+func MapToString(m map[string]string) string {
 	var str string
 	for key, value := range m {
 		str += key + "=" + value + "&"
 	}
 	return strings.TrimRight(str, "&")
+}
+
+// ToSnakeCase is a utility function that converts a given string into snake case.
+func ToSnakeCase(s string) string {
+	var buffer bytes.Buffer
+	for i, r := range s {
+		if unicode.IsUpper(r) {
+			if i > 0 {
+				buffer.WriteRune('_')
+			}
+			buffer.WriteRune(unicode.ToLower(r))
+		} else {
+			buffer.WriteRune(r)
+		}
+	}
+	return buffer.String()
 }
--- util_test.go
+++ util_test.go
@@ -3,10 +3,50 @@ package orm
 import "testing"
 
 func TestMapToString(t *testing.T) {
-	m := map[string]string{"key1": "value1", "key2": "value2"}
-	got := mapToString(m)
-	want := "key1=value1&key2=value2"
-	if got != want {
-		t.Errorf("mapToString(%v) = %q, want %q", m, got, want)
+	testCases := []struct {
+		name     string
+		input    map[string]string
+		expected string
+	}{
+		{
+			name:     "Test Case 1",
+			input:    map[string]string{"key1": "value1", "key2": "value2"},
+			expected: "key1=value1&key2=value2",
+		},
+		{
+			name:     "Test Case 2",
+			input:    map[string]string{"key": "value"},
+			expected: "key=value",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := MapToString(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %s, got %s", tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestToSnakeCase(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"All lower case", "testcase", "testcase"},
+		{"Camel case", "testCase", "test_case"},
+		{"With numbers", "testCase123", "test_case123"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := ToSnakeCase(tt.input)
+			if result != tt.expected {
+				t.Errorf("ToSnakeCase(%q) = %q, want %q", tt.input, result, tt.expected)
+			}
+		})
 	}
 }
